4ee9dbf1e380fbf43fa177ce550f67fe297671e3
c9u11 <tjrdud6412@naver.com>
2023-08-25 05:20:59 +0900

    ✏️

--- src/api/BannersAPI.ts
+++ src/api/BannersAPI.ts
@@ -1,25 +1,5 @@
-import {
-  BASE_URL,
-  GET_ACCEPTS_STATUS_CATEGORIES,
-  GET_BLOOD_DONATION_CATEGORIES,
-  GET_BLOOD_TYPE_CATEGORIES,
-  GET_QUESTION_TYPE_CATEGORIES,
-  GET_REQUESTS_STATUS_CATEGORIES,
-} from "constants/Api";
+import { BASE_URL, GET_BANNERS } from "constants/Api";
 import axios from "axios";
-
-const GetAcceptsStatusCategories = () => {
-  return axios.get(BASE_URL + GET_ACCEPTS_STATUS_CATEGORIES);
-};
-const GetBloodDonationCategories = () => {
-  return axios.get(BASE_URL + GET_BLOOD_DONATION_CATEGORIES);
-};
-const GetBloodTypeCategories = () => {
-  return axios.get(BASE_URL + GET_BLOOD_TYPE_CATEGORIES);
-};
-const GetQuestionTypeCategories = () => {
-  return axios.get(BASE_URL + GET_QUESTION_TYPE_CATEGORIES);
-};
-const GetRequestsStatusCategories = () => {
-  return axios.get(BASE_URL + GET_REQUESTS_STATUS_CATEGORIES);
+const GetBanners = () => {
+  return axios.get(BASE_URL + GET_BANNERS);
 };
