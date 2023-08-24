9887c0a8da6f836de37b3b0375281f005514dc22
c9u11 <tjrdud6412@naver.com>
2023-08-24 19:46:17 +0900

    remove content title

--- src/pages/Rank/index.jsx
+++ src/pages/Rank/index.jsx
@@ -1,10 +1,9 @@
-import { ContentContainer, ContentTitle } from "components/Content";
+import { ContentContainer } from "components/Content";
 import { List, ListItem, SubTitle } from "./styled";
 
 function Rank() {
   return (
     <ContentContainer>
-      <ContentTitle>명예의 전당</ContentTitle>
       <List>
         <ListItem title="XXX위" name="황석영" span="헌혈 총 nn회" isMine />
         <SubTitle>TOP 100</SubTitle>
